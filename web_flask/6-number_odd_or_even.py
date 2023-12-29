602787991c56600adf8794e6eb9e54399d5a28af
Mitchkal <mitchellkalenda@gmail.com>
2023-12-29 07:52:10 -0500

    6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -52,5 +52,12 @@ def number_template(n):
     return (render_template('5-number.html', number=n))
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def display_even_odd(n):
+    """displays page with if n is even or odd"""
+    parity = 'even' if (n % 2 == 0) else 'odd'
+    return (render_template('6-number_odd_or_even.html', number=n, parity=parity))
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
