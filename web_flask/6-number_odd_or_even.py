53d1228c4f32563bee04c0942adf6ca9771dca88
Mitchkal <mitchellkalenda@gmail.com>
2023-12-29 07:57:38 -0500

    fixed pycodestyle

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -56,7 +56,8 @@ def number_template(n):
 def display_even_odd(n):
     """displays page with if n is even or odd"""
     parity = 'even' if (n % 2 == 0) else 'odd'
-    return (render_template('6-number_odd_or_even.html', number=n, parity=parity))
+    return (render_template('6-number_odd_or_even.html',
+            number=n, parity=parity))
 
 
 if __name__ == "__main__":
