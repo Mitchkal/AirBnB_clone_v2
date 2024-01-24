1fb7d322ca596a5ed4ff52402e56ce789b3e1ad7
Mitchkal <mitchellkalenda@gmail.com>
2024-01-24 10:45:41 -0500

    fixed 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,24 +11,40 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
+"""@app.route('/states', strict_slashes=False)
 def states():
-    """displays cities by states"""
+    displays cities by states
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('9-states.html', states=sorted_states)
+    state_list = [state for state in sorted_states]
+    return render_template('9-states.html', states=state_list)"""
 
 
+@app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
-def state_detail(id):
+def state_detail(id=None):
     """displays state details"""
+    states = storage.all('State')
+    if id:
+        key = '{}.{}'.format('State', id)
+        if key in states:
+            states = states[key]
+        else:
+            states = None
+    else:
+        states = storage.all('State').values()
+
+    return render_template('9-states.html', states=states, id=id)
+    """
     states = storage.all(State).values()
     state = next((state for state in states if state.id == id), None)
     if state:
-        cities = sorted(state.cities, key=lambda x: x.name) if state else []
-        return render_template('9-states.html', state=state, cities=cities)
+        cities = sorted(state.cities, key=lambda x: x.name) \
+                if state.cities else []
+        return render_template('9-states.html', states=state, cities=cities)
     else:
         return render_template('9-states.html', states=None)
+"""
 
 
 @app.teardown_appcontext
